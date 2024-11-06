3551491f4d5cde3ab048b09794142e2eb70574fd
mohammadshivaii <mohammadshivaii4@gmail.com>
2024-11-06 00:04:12 -0800

    Add files via upload

---
+++ # تابع برای کامل بودن عدn.py
@@ -0,0 +1,61 @@
+# # تابع برای کامل بودن عدn 
+# def perfect(n):
+#     sum = 0
+#     for  i in range(1,N // 2+i):
+#         if N % 1 == 0:
+#             sum = sum + i
+#     if N == sum :
+#         return True
+#     else:
+#         return False
+# perfect(10)
+# # یافتن عدد کامل با کمک تابع
+# N = int(input())
+# for i in range(1,n+1):
+#     if perfect(i):
+#         print(i)
+
+
+
+
+import random
+# for i in range (10):
+#     print(random.random())
+
+
+
+
+# for i in range(20):
+#     print(random.randint(10,20))
+
+
+
+# # شبیه سازی پرتاب تاس 
+# c1 = c2 = c3 = c4 = c5 = c6 = 0
+# from random import randint
+# n = 1200
+# for i in range(n):
+#     k = randint(1,6)
+#     if k == i :
+#         c1 = c1 + i
+#     elif k ==2:
+#         c2 = c2 + i
+#     elif k == c3:
+#         c3 = c3 + i
+#     elif k == c4:
+#         c4 = c4 + i
+#     elif k == c5:
+#         c5 = c5 + i
+#     else:
+#         c6 = c6 + i
+# print(f"1 -----> {c1} ---->{c1/n:.4f}")
+# print(f"2 -----> {c2} ---->{c2/n:.4f}")
+# print(f"3 -----> {c3} ---->{c3/n:.4f}")
+# print(f"4 -----> {c4} ---->{c4/n:.4f}")
+# print(f"5 -----> {c5} ---->{c5/n:.4f}")
+# print(f"6 -----> {c6} ---->{c6/n:.4f}")
+
+
+
+
+
